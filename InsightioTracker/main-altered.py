6417ebe93c79ff5dddde949d8383cbb859c90608
gunalpgungor <gnlpgngr@gmail.com>
2023-12-30 12:28:36 +0300

    Drawing the Box
    removed MangoDB related code
    Defined a BoxZone Class
    Replaced the Line Annotator with Box Annotator
    Updated the video processing thread to comply with the box

--- InsightioTracker/main-altered.py
+++ InsightioTracker/main-altered.py
@@ -32,18 +32,34 @@ import PIL.Image
 import PIL.ImageTk
 
 
-# Connect to MongoDB
-mongo_url = config["mongo_url"]
-mongo_client = pymongo.MongoClient(mongo_url)
-mongo_db = mongo_client["smart_ankara"]
-mongo_collection_counter = mongo_db["bike_counter"]
-mongo_collection_hrecords = mongo_db["daily_record"]
+# Define the BoxZone Class
+class BoxZone:
+    def __init__(self, top_left, bottom_right):
+        self.top_left = top_left
+        self.bottom_right = bottom_right
+        self.in_count = 0
+        self.out_count = 0
+
+    def is_inside(self, detection):
+        x_center = (detection[0] + detection[2]) / 2
+        y_center = (detection[1] + detection[3]) / 2
+        return (self.top_left.x <= x_center <= self.bottom_right.x) and \
+               (self.top_left.y <= y_center <= self.bottom_right.y)
+
+    def update_counters(self, detections):
+        for detection in detections.xyxy:
+            if self.is_inside(detection):
+                self.in_count += 1
+            else:
+                self.out_count += 1
+
 
 # Connect to API
 api_post_url = config["api_post_url"]
 api_token = "bee429fb-569f-46d1-bd5d-e999a6ff750c"
 sayac = config["sayac"]
 api_get_url = config["api_get_url"] + sayac
+
 
 @dataclass(frozen=True)
 class BYTETrackerArgs:
@@ -54,12 +70,14 @@ class BYTETrackerArgs:
     min_box_area: float = 1.0
     mot20: bool = False
 
+
 # Converts Detections into format that can be consumed by match_detections_with_tracks function
 def detections2boxes(detections: sv.Detections) -> np.ndarray:
     return np.hstack((
         detections.xyxy,
         detections.confidence[:, np.newaxis]
     ))
+
 
 # Converts List[STrack] into format that can be consumed by match_detections_with_tracks function
 def tracks2boxes(tracks: List[STrack]) -> np.ndarray:
@@ -90,6 +108,7 @@ def match_detections_with_tracks(
 
     return tracker_ids
 
+
 MODEL = "yolov8x.pt"
 model = YOLO(MODEL)
 model.fuse()
@@ -102,15 +121,11 @@ else:
     print("cpu")
 model.to(device)
 
-# Dict maping class_id to class_name
+# Dict mapping class_id to class_name
 CLASS_NAMES_DICT = model.model.names
 
 # Class_ids of interest - bicycle, person
 CLASS_ID = [1]
-
-# Line settings
-LINE_START = sv.Point(config["line_start_x"], config["line_start_y"])
-LINE_END = sv.Point(config["line_end_x"], config["line_end_y"])
 
 # cap = cv2.VideoCapture(config["rtsp_address"], cv2.CAP_FFMPEG)
 cap = cv2.VideoCapture(0)
@@ -118,21 +133,24 @@ cap = cv2.VideoCapture(0)
 # Create BYTETracker instance
 byte_tracker = BYTETracker(BYTETrackerArgs())
 
-# Create LineCounter instance
-line_counter = sv.LineZone(start=LINE_START, end=LINE_END)
-
 # Create instance of BoxAnnotator and LineCounterAnnotator
-box_annotator = sv.BoxAnnotator(color=sv.Color(255, 0, 0), thickness=2, text_thickness=2, text_scale=2)
-line_annotator = sv.LineZoneAnnotator(thickness=2, text_thickness=2, text_scale=2)
+box_annotator = sv.BoxAnnotator(color=sv.Color(0, 0, 255), thickness=2, text_thickness=2, text_scale=2)
+
+# Box settings (replace these with actual coordinates from your config)
+BOX_TOP_LEFT = sv.Point(100, 100)
+BOX_BOTTOM_RIGHT = sv.Point(1150, 650)
+
+# Create BoxZone instance
+box_zone = BoxZone(BOX_TOP_LEFT, BOX_BOTTOM_RIGHT)
+
 
 # Video processing thread
 def video_processing_thread():
     camera_connected = False
-    
+
     while True:
         # If the camera is not connected, try to reconnect
         if not camera_connected:
-            # cap = cv2.VideoCapture(config["rtsp_address"], cv2.CAP_FFMPEG)
             cap = cv2.VideoCapture(0)
             if cap.isOpened():
                 camera_connected = True
@@ -141,58 +159,47 @@ def video_processing_thread():
             ret, frame = cap.read()
             if not ret:
                 print("Camera disconnected")
-                # If the camera is disconnected, set camera_connected to False
                 camera_connected = False
                 continue
 
-            # Model prediction on single frame and conversion to supervision Detections
+            # Model prediction and detection tracking
             results = model(frame)
             detections = sv.Detections(
                 xyxy=results[0].boxes.xyxy.cpu().numpy(),
                 confidence=results[0].boxes.conf.cpu().numpy(),
                 class_id=results[0].boxes.cls.cpu().numpy().astype(int)
             )
-
-            # Filtering out detections with unwanted classes
             mask = np.array([class_id in CLASS_ID for class_id in detections.class_id], dtype=bool)
             detections = detections[mask]
-
-            # Tracking detections
-            tracks = byte_tracker.update(
-                output_results=detections2boxes(detections=detections),
-                img_info=frame.shape,
-                img_size=frame.shape
-            )
+            tracks = byte_tracker.update(output_results=detections2boxes(detections=detections), img_info=frame.shape,
+                                         img_size=frame.shape)
             tracker_id = match_detections_with_tracks(detections=detections, tracks=tracks)
             detections.tracker_id = np.array(tracker_id)
-
-            # Filtering out detections without trackers
             mask = np.array([tracker_id is not None for tracker_id in detections.tracker_id], dtype=bool)
             detections = detections[mask]
 
-
             # Format custom labels
-            labels = [
-                f"#{tracker_id} {CLASS_NAMES_DICT[class_id]} {confidence:0.2f}"
-                for _, _, confidence, class_id, tracker_id
-                in detections
-            ]
+            labels = [f"#{tracker_id} {CLASS_NAMES_DICT[class_id]} {confidence:0.2f}" for
+                      _, _, confidence, class_id, tracker_id in detections]
 
-            # Updating line counter
-            in_count, out_count = line_counter.trigger(detections=detections)
+            # Update BoxZone counters
+            box_zone.update_counters(detections)
 
-            # Annotate and display frame
-            box_annotated_frame = box_annotator.annotate(scene=frame.copy(), detections=detections, labels=labels)
-            all_annotated_frame = line_annotator.annotate(frame=box_annotated_frame, line_counter=line_counter)
-            total = in_count + out_count
+            # Draw the box on the frame
+            cv2.rectangle(frame, (BOX_TOP_LEFT.x, BOX_TOP_LEFT.y), (BOX_BOTTOM_RIGHT.x, BOX_BOTTOM_RIGHT.y),
+                          (0, 255, 0), 2)
+            cv2.putText(frame, f"In: {box_zone.in_count}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            cv2.putText(frame, f"Out: {box_zone.out_count}", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-            cv2.imshow("yolov8", all_annotated_frame)
+            # Display the annotated frame
+            cv2.imshow("yolov8", frame)
 
             if cv2.waitKey(1) == ord('q'):
                 break
 
     cap.release()
     cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     
