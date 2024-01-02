1a13481e1dda9c063c07d410babb51f2d9a43963
gunalpgungor <gnlpgngr@gmail.com>
2024-01-02 04:50:24 +0300

    reverting to original
    could not make the code work as intended

--- InsightioTracker/main-altered.py
+++ InsightioTracker/main-altered.py
@@ -23,28 +23,6 @@ from ultralytics import YOLO
 from typing import List
 
 
-# Define the BoxZone Class
-class BoxZone:
-    def __init__(self, top_left, bottom_right):
-        self.top_left = top_left
-        self.bottom_right = bottom_right
-        self.in_count = 0
-        self.out_count = 0
-
-    def is_inside(self, detection):
-        x_center = (detection[0] + detection[2]) / 2
-        y_center = (detection[1] + detection[3]) / 2
-        return (self.top_left.x <= x_center <= self.bottom_right.x) and \
-               (self.top_left.y <= y_center <= self.bottom_right.y)
-
-    def update_counters(self, detections):
-        for detection in detections.xyxy:
-            if self.is_inside(detection):
-                self.in_count += 1
-            else:
-                self.out_count += 1
-
-
 @dataclass(frozen=True)
 class BYTETrackerArgs:
     track_thresh: float = 0.25
@@ -74,8 +52,8 @@ def tracks2boxes(tracks: List[STrack]) -> np.ndarray:
 
 # Matches our bounding boxes with predictions
 def match_detections_with_tracks(
-    detections: sv.Detections,
-    tracks: List[STrack]
+        detections: sv.Detections,
+        tracks: List[STrack]
 ) -> sv.Detections:
     if not np.any(detections.xyxy) or len(tracks) == 0:
         return np.empty((0,))
@@ -105,22 +83,15 @@ else:
     print("cpu")
 model.to(device)
 
-# # Dict mapping class_id to class_name
-# CLASS_NAMES_DICT = model.model.names
-
-CLASS_NAMES_DICT = [
-    "bicycle", "person", "car", "motorcycle", "airplane",
-    "bus", "train", "truck", "boat", "traffic light",
-    # ... more class names ...
-]
-
-# Print the list of class names, debugging purposes
-print("List of class names:")
-for i, class_name in enumerate(CLASS_NAMES_DICT):
-    print(f"Class ID {i}: {class_name}")
+# Dict maping class_id to class_name
+CLASS_NAMES_DICT = model.model.names
 
 # Class_ids of interest - bicycle, person
 CLASS_ID = [1]
+
+# Line settings
+LINE_START = sv.Point(config["line_start_x"], config["line_start_y"])
+LINE_END = sv.Point(config["line_end_x"], config["line_end_y"])
 
 # cap = cv2.VideoCapture(config["rtsp_address"], cv2.CAP_FFMPEG)
 cap = cv2.VideoCapture(0)
@@ -128,15 +99,12 @@ cap = cv2.VideoCapture(0)
 # Create BYTETracker instance
 byte_tracker = BYTETracker(BYTETrackerArgs())
 
+# Create LineCounter instance
+line_counter = sv.LineZone(start=LINE_START, end=LINE_END)
+
 # Create instance of BoxAnnotator and LineCounterAnnotator
-box_annotator = sv.BoxAnnotator(color=sv.Color(0, 0, 255), thickness=2, text_thickness=2, text_scale=2)
-
-# Box settings (replace these with actual coordinates from your config)
-BOX_TOP_LEFT = sv.Point(100, 100)
-BOX_BOTTOM_RIGHT = sv.Point(600, 650)
-
-# Create BoxZone instance
-box_zone = BoxZone(BOX_TOP_LEFT, BOX_BOTTOM_RIGHT)
+box_annotator = sv.BoxAnnotator(color=sv.Color(255, 0, 0), thickness=2, text_thickness=2, text_scale=2)
+line_annotator = sv.LineZoneAnnotator(thickness=2, text_thickness=2, text_scale=2)
 
 
 # Video processing thread
@@ -144,55 +112,62 @@ def video_processing_thread():
     camera_connected = False
 
     while True:
+        # If the camera is not connected, try to reconnect
         if not camera_connected:
+            # cap = cv2.VideoCapture(config["rtsp_address"], cv2.CAP_FFMPEG)
             cap = cv2.VideoCapture(0)
             if cap.isOpened():
                 camera_connected = True
         else:
+            # Capture a frame from the webcam
             ret, frame = cap.read()
             if not ret:
                 print("Camera disconnected")
+                # If the camera is disconnected, set camera_connected to False
                 camera_connected = False
                 continue
 
+            # Model prediction on single frame and conversion to supervision Detections
             results = model(frame)
             detections = sv.Detections(
                 xyxy=results[0].boxes.xyxy.cpu().numpy(),
                 confidence=results[0].boxes.conf.cpu().numpy(),
                 class_id=results[0].boxes.cls.cpu().numpy().astype(int)
             )
+
+            # Filtering out detections with unwanted classes
             mask = np.array([class_id in CLASS_ID for class_id in detections.class_id], dtype=bool)
             detections = detections[mask]
-            tracks = byte_tracker.update(output_results=detections2boxes(detections=detections), img_info=frame.shape,
-                                         img_size=frame.shape)
+
+            # Tracking detections
+            tracks = byte_tracker.update(
+                output_results=detections2boxes(detections=detections),
+                img_info=frame.shape,
+                img_size=frame.shape
+            )
             tracker_id = match_detections_with_tracks(detections=detections, tracks=tracks)
             detections.tracker_id = np.array(tracker_id)
+
+            # Filtering out detections without trackers
             mask = np.array([tracker_id is not None for tracker_id in detections.tracker_id], dtype=bool)
             detections = detections[mask]
 
-            # Debugging: Print number of detections
-            print(f"Number of detections: {len(detections.xyxy)}")
+            # Format custom labels
+            labels = [
+                f"#{tracker_id} {CLASS_NAMES_DICT[class_id]} {confidence:0.2f}"
+                for _, _, confidence, class_id, tracker_id
+                in detections
+            ]
 
-            for det in detections.xyxy:
-                x1, y1, x2, y2, conf, class_id = det
-                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
-                label = f"{CLASS_NAMES_DICT[int(class_id)]}: {conf:.2f}"
-                cv2.putText(frame, label, (int(x1), int(y1 - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            # Updating line counter
+            in_count, out_count = line_counter.trigger(detections=detections)
 
-                # Debugging: Print each detection
-                print(f"Detection: {label}")
+            # Annotate and display frame
+            box_annotated_frame = box_annotator.annotate(scene=frame.copy(), detections=detections, labels=labels)
+            all_annotated_frame = line_annotator.annotate(frame=box_annotated_frame, line_counter=line_counter)
+            total = in_count + out_count
 
-            box_zone.update_counters(detections)
-
-            # Debugging: Print in and out counts
-            print(f"In count: {box_zone.in_count}, Out count: {box_zone.out_count}")
-
-            cv2.rectangle(frame, (BOX_TOP_LEFT.x, BOX_TOP_LEFT.y), (BOX_BOTTOM_RIGHT.x, BOX_BOTTOM_RIGHT.y),
-                          (0, 255, 0), 2)
-            cv2.putText(frame, f"In: {box_zone.in_count}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-            cv2.putText(frame, f"Out: {box_zone.out_count}", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-            cv2.imshow("yolov8", frame)
+            cv2.imshow("yolov8", all_annotated_frame)
 
             if cv2.waitKey(1) == ord('q'):
                 break
@@ -200,9 +175,10 @@ def video_processing_thread():
     cap.release()
     cv2.destroyAllWindows()
 
-# Main program execution
+
 if __name__ == "__main__":
     video_thread = threading.Thread(target=video_processing_thread)
-    video_thread.daemon = True
+    video_thread.daemon = True  # The thread will terminate when the main program exits
     video_thread.start()
+
     video_thread.join()
