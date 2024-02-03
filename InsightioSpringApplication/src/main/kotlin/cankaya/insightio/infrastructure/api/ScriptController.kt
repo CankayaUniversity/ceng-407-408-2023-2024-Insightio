4e65bc9c315532248eea61b12de827e0b04b8227
Ekin Nalçacı <ekinnalcaci@yahoo.com>
2024-02-03 19:56:03 +0300

    added script running to tracker service

--- InsightioSpringApplication/src/main/kotlin/cankaya/insightio/infrastructure/api/ScriptController.kt
+++ InsightioSpringApplication/src/main/kotlin/cankaya/insightio/infrastructure/api/ScriptController.kt
@@ -1,5 +1,10 @@
 package cankaya.insightio.infrastructure.api
 
+import cankaya.insightio.application.services.TrackerService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import cankaya.insightio.application.services.UserService
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
@@ -8,11 +13,12 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/deneme")
-class TestController {
-    @GetMapping("/deneme/{id}")
-    fun getResourceById(@PathVariable id: Long): String {
-        val resource = "Resource with ID $id"
-        return resource
+class ScriptController(
+        val trackerService: TrackerService
+) {
+    @GetMapping("/run")
+    suspend fun runPythonScript() {
+        return trackerService.runPythonScript()
     }
 }
 
