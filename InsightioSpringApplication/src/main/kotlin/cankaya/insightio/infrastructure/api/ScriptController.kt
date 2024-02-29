5f81447877c154aff1833cfc458cb35814b482f0
edanuraltnk <120946670+edanuraltnk@users.noreply.github.com>
2024-02-29 20:23:17 +0300

    Fixed Error ScriptController.kt

--- InsightioSpringApplication/src/main/kotlin/cankaya/insightio/infrastructure/api/ScriptController.kt
+++ InsightioSpringApplication/src/main/kotlin/cankaya/insightio/infrastructure/api/ScriptController.kt
@@ -27,17 +27,4 @@ class ScriptController(
 }
 
 
-//Login Controller
-@RestController
-@RequestMapping("/login")
-class UserController(private val userService: UserService) {
 
-    @GetMapping("/{login}")
-    fun getUserByLogin(@PathVariable username: String, @RequestParam password: String): ResponseEntity<String> {
-        return if (userService.validateUser(username, password)) {
-            ResponseEntity.ok("Login successful")
-        } else {
-            ResponseEntity.status(401).body("Unauthorized")
-        }
-    }
-}
