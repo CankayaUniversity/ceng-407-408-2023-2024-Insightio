e9d4077da9998c2806245f15b5bd4695df8b32f6
rev <uozbek29@outlook.com>
2024-03-04 20:48:32 +0300

    Added CORS custom configurations, connected dashboard to server

--- InsightioUI/insightio-ui/src/renderer/src/components/pages/Dashboard.svelte
+++ InsightioUI/insightio-ui/src/renderer/src/components/pages/Dashboard.svelte
@@ -6,6 +6,9 @@
   import clsx from 'clsx'
   import Button from '../utility/Button.svelte'
   import CountHelper from '../../functions/countData'
+  import { getAllCameraSettings } from '../../api/cameras'
+  import Spinner from '../utility/Spinner.svelte'
+  import pageStore from '../../stores/pageStore'
 
   // TODO: Replace with actual data
   const mockData = [
@@ -75,18 +78,20 @@
       ]
     }
   ]
-  let selectedCameraId = '65e362908a09bd593f34a256'
 
   const targets = mockData.map((o) => o.target)
   const chartTypes = ['bar', 'line', 'pie', 'doughnut', 'radar']
-  const maxCameraIndex = 4
 
+  let selectedCameraSetting
+  let cameraSettings = []
   let currentTimeFrame = 'day'
   let currentCameraIndex = 0
   let currentChartTypeIndex = 0
   let timeVisibility = CountHelper.initTimeVisibilityArray(currentTimeFrame)
   let currentChartData = CountHelper.getFramedCountData(mockData, currentTimeFrame)
   let currentChartType = chartTypes[currentChartTypeIndex]
+  let maxCameraIndex
+  let showLoading = false
 
   function updateTimeFrame(timeframe) {
     currentTimeFrame = timeframe
@@ -95,8 +100,12 @@
   }
 
   function navigateCamera(direction) {
-    // not implemented
-    console.log(direction)
+    if (direction === 'next' && currentCameraIndex != maxCameraIndex) {
+      currentCameraIndex += 1
+    } else if (direction === 'prev' && currentCameraIndex != 0) {
+      currentCameraIndex -= 1
+    }
+    selectedCameraSetting = cameraSettings[currentCameraIndex]
   }
 
   function navigateChart(direction) {
@@ -116,83 +125,117 @@
     console.log(isVisible)
     return isVisible
   }
+
+  async function fetchSettings() {
+    showLoading = true
+    cameraSettings = await getAllCameraSettings()
+    let camerasExist = cameraSettings.length > 0 ? true : false
+    console.log(cameraSettings)
+    maxCameraIndex = camerasExist ? cameraSettings.length - 1 : 0
+    selectedCameraSetting = camerasExist ? cameraSettings[0] : null
+    showLoading = false
+  }
+
+  $: $pageStore.activePage === 'dashboard' && fetchSettings()
 </script>
 
-<div class="flex flex-col h-full w-full bg-gray-800 text-white">
-  <!-- Dashboard Header -->
-  <div class="px-4 pt-4 flex justify-between items-center">
-    <h1 class="text-5xl font-bold">Dashboard</h1>
-    <div></div>
-  </div>
-
-  <!-- Camera Navigation -->
-  <div class="flex justify-center py-2">
-    <Button on:click={() => navigateCamera('prev')}>
-      <Icon icon="arrowLeft" highlightOnHover class="h-5 w-5" />
-    </Button>
-    <div class="flex items-center justify-center">
-      <!-- eslint-disable-next-line no-unused-vars -->
-      {#each Array(maxCameraIndex + 1) as _, i (i)}
-        <span class="inline-block mx-1">
-          <Icon
-            icon="circleFull"
-            highlightOnHover
-            class="h-4 w-4"
-            style={clsx(currentCameraIndex === i && 'opacity: 1.0 !important;')}
-          />
-        </span>
-      {/each}
+{#if !showLoading}
+  <div class="flex flex-col h-full w-full bg-gray-800 text-white">
+    <!-- Dashboard Header -->
+    <div class="px-4 pt-4 flex justify-between items-center">
+      <h1 class="text-5xl font-bold">Dashboard</h1>
+      <div></div>
     </div>
-    <Button on:click={() => navigateCamera('next')}>
-      <Icon icon="arrowRight" highlightOnHover class="h-5 w-5" />
-    </Button>
-  </div>
 
-  <!-- Dashboard Content -->
-  <div class="flex flex-grow">
-    <!-- Chart Section -->
-    <div class="flex flex-col w-1/2 p-4 ml-8">
-      <div class="flex flex-row pl-3">
-        <Icon icon="chart" class="w-10 h-10" />
-        <h2 class="text-3xl pl-3 font-bold">Charts</h2>
-      </div>
-      <div class="mt-6 flex items-center justify-between">
-        <Button on:click={() => navigateChart('prev')}>
-          <Icon icon="arrowLeft" highlightOnHover class="h-7 w-7" />
+    {#if cameraSettings.length > 0}
+      <!-- Camera Navigation -->
+      <div class="flex justify-center py-2">
+        <Button on:click={() => navigateCamera('prev')}>
+          <Icon icon="arrowLeft" highlightOnHover class="h-5 w-5" />
         </Button>
-        <div class="flex-grow">
-          <div class="flex flex-row">
-            <p class="text-base mt-1 mr-3">Time Frame:</p>
-            <Dropdown
-              items={CountHelper.validTimeFrames}
-              selectedItem={CountHelper.validTimeFrames[0]}
-              on:change={(e) => updateTimeFrame(e.detail.value)}
-            />
+        <div class="flex items-center justify-center">
+          <!-- eslint-disable-next-line no-unused-vars -->
+          {#each Array(maxCameraIndex + 1) as _, i (i)}
+            <!-- svelte-ignore a11y-no-static-element-interactions -->
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
+            <span
+              class="inline-block mx-1"
+              on:click={() => {
+                currentCameraIndex = i
+                selectedCameraSetting = cameraSettings[i]
+              }}
+            >
+              <Icon
+                icon="circleFull"
+                highlightOnHover
+                class="h-4 w-4"
+                style={clsx(currentCameraIndex === i && 'opacity: 1.0 !important;')}
+              />
+            </span>
+          {/each}
+        </div>
+        <Button on:click={() => navigateCamera('next')}>
+          <Icon icon="arrowRight" highlightOnHover class="h-5 w-5" />
+        </Button>
+      </div>
+    {/if}
+
+    <!-- Dashboard Content -->
+    <div class="flex flex-grow">
+      {#if cameraSettings.length > 0}
+        <!-- Chart Section -->
+        <div class="flex flex-col w-1/2 p-4 ml-8">
+          <div class="flex flex-row pl-3">
+            <Icon icon="chart" class="w-10 h-10" />
+            <h2 class="text-3xl pl-3 font-bold">Charts</h2>
           </div>
-          <!-- Chart Component -->
-          <Chart
-            bind:currentChartData
-            bind:chartType={currentChartType}
-            visibilityCallback={filterLegendChosenTime}
-            {targets}
-          />
+          <div class="mt-6 flex items-center justify-between">
+            <Button on:click={() => navigateChart('prev')}>
+              <Icon icon="arrowLeft" highlightOnHover class="h-7 w-7" />
+            </Button>
+            <div class="flex-grow">
+              <div class="flex flex-row">
+                <p class="text-base mt-1 mr-3">Time Frame:</p>
+                <Dropdown
+                  items={CountHelper.validTimeFrames}
+                  selectedItem={CountHelper.validTimeFrames[0]}
+                  on:change={(e) => updateTimeFrame(e.detail.value)}
+                />
+              </div>
+              <!-- Chart Component -->
+              <Chart
+                bind:currentChartData
+                bind:chartType={currentChartType}
+                visibilityCallback={filterLegendChosenTime}
+                {targets}
+              />
+            </div>
+            <Button on:click={() => navigateChart('next')}>
+              <Icon icon="arrowRight" highlightOnHover class="h-7 w-7" />
+            </Button>
+          </div>
         </div>
-        <Button on:click={() => navigateChart('next')}>
-          <Icon icon="arrowRight" highlightOnHover class="h-7 w-7" />
-        </Button>
-      </div>
-    </div>
 
-    <!-- Camera View Section with fixed size -->
-    <div class="w-1/2 flex justify-center p-4 mr-8">
-      <div class="w-full max-w-lg">
-        <div class="flex flex-row gap-3">
-          <Icon icon="camera" class="w-10 h-10" />
-          <h2 class="text-3xl mb-6 font-bold">Camera View</h2>
+        <!-- Camera View Section with fixed size -->
+        <div class="w-1/2 flex justify-center p-4 mr-8">
+          <div class="w-full max-w-lg">
+            <div class="flex flex-row gap-3">
+              <Icon icon="camera" class="w-10 h-10" />
+              <h2 class="text-3xl mb-6 font-bold">Camera View</h2>
+            </div>
+
+            {#if selectedCameraSetting}
+              <CameraView bind:cameraId={selectedCameraSetting.id} />
+            {/if}
+          </div>
         </div>
-
-        <CameraView bind:cameraId={selectedCameraId} />
-      </div>
+      {:else}
+        <div class="flex flex-grow items-center justify-center">
+          <h1 class="text-3xl font-bold">No cameras to show</h1>
+        </div>
+      {/if}
     </div>
   </div>
-</div>
+{:else}
+  <Spinner />
+{/if}
---
+++ InsightioUI/insightio-ui/src/renderer/src/components/utility/Spinner.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let size = 50 // default size of the spinner
+</script>
+
+<div class="loading-container">
+  <div class="spinner" style="width: {size}px; height: {size}px;"></div>
+</div>
+
+<style>
+  .loading-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%; /* or any other size you want */
+  }
+
+  .spinner {
+    border: 4px solid rgba(255, 255, 255, 0.3);
+    border-radius: 50%;
+    border-top-color: #fff;
+    animation: spin 1s ease-in-out infinite;
+  }
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+</style>
