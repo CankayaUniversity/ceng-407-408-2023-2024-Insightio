55bfa855c0479de63e4e1ccdaeeeed570e5b932a
Eda Nur Altunok <altunokedanur@gmail.com>
2024-05-23 13:34:31 +0300

    Added dependencies for CameraController.kt and formatted

--- InsightioSpringApplication/src/main/kotlin/cankaya/insightio/infrastructure/api/CameraController.kt
+++ InsightioSpringApplication/src/main/kotlin/cankaya/insightio/infrastructure/api/CameraController.kt
@@ -2,8 +2,8 @@ package cankaya.insightio.infrastructure.api
 
 import cankaya.insightio.application.services.CameraService
 import cankaya.insightio.domain.Camera
-import cankaya.insightio.infrastructure.mongodb.impls.CameraStatus
-import cankaya.insightio.infrastructure.mongodb.impls.CameraType
+import cankaya.insightio.domain.CameraStatus
+import cankaya.insightio.domain.CameraType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
